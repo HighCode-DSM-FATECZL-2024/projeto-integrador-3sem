859319e5031349134c36eda17f212791e1e226ff
mattera-dev <vinimatteraz@gmail.com>
2025-06-28 12:13:25 -0300

    feat: add cors middleware to work with frontend in another origin ( better to development )

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.handlers.exception_handlers import register_exception_handlers
 from app.modules.users.routes import users as users_route
@@ -8,6 +9,13 @@ def create_app(title: str,version: str) -> FastAPI:
     app = FastAPI(title=title, version=version)
 
     register_exception_handlers(app)
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
     app.include_router(auth_route, prefix="/api")
     app.include_router(users_route, prefix="/api")
