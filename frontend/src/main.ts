5fe5444d7b5233c6b0d0a61b786689273d8d7ade
mattera-dev <vinimatteraz@gmail.com>
2025-06-28 12:19:41 -0300

    feat: create view of login and register and add working register with error validating, add component of inputs

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { MaskInput } from 'vue-3-mask'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.component('MaskInput', MaskInput)
 app.use(createPinia())
 app.use(router)
 
